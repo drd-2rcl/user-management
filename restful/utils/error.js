4dcc372236809f47b6145a5754dfdbc003f8dc14
drd-2rcl <drd.rclrd@gmail.com>
2019-03-13 21:55:18 -0300

    client-server, config to bower-components and express generator

---
+++ restful/utils/error.js
@@ -0,0 +1,11 @@
+module.export = {
+    send: (err, req, res, code = 400) => {
+
+        console.log(`error: ${err}`);
+    
+        res.status(code).json({
+            error:err
+        });
+
+    }
+};
